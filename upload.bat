c85d3f38bbfc13baee0b55d04f02f22192c8ba54
Hao Huynh <hah.ptit@gmail.com>
2015-10-25 19:17:03 -0700

    PokerGame: 20%

--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
 git add --all
 git config --global user.name "Hao Huynh"
 git config --global user.email hah.ptit@gmail.com
-git commit -m "PokerGame: Fixed Flashing Issue"
+git commit -m "PokerGame: 20%"
 git push origin master
