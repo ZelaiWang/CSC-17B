eb39b9a3995e2969aace90640673d437fff7a3af
Hao Huynh <hah.ptit@gmail.com>
2015-12-09 10:31:45 -0800

    Flapping Bird v2: Interleaved flowers

--- Projects/build-PokerGame-Desktop_Qt_5_5_0_MinGW_32bit-Debug/debug/moc_uicontroller.cpp
+++ Projects/build-PokerGame-Desktop_Qt_5_5_0_MinGW_32bit-Debug/debug/moc_uicontroller.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_UIController_t {
-    QByteArrayData data[9];
-    char stringdata0[88];
+    QByteArrayData data[12];
+    char stringdata0[130];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,12 +37,16 @@ QT_MOC_LITERAL(4, 37, 15), // "processBetValue"
 QT_MOC_LITERAL(5, 53, 7), // "newGame"
 QT_MOC_LITERAL(6, 61, 8), // "saveGame"
 QT_MOC_LITERAL(7, 70, 8), // "loadGame"
-QT_MOC_LITERAL(8, 79, 8) // "exitGame"
+QT_MOC_LITERAL(8, 79, 8), // "exitGame"
+QT_MOC_LITERAL(9, 88, 12), // "stateChanged"
+QT_MOC_LITERAL(10, 101, 19), // "QMediaPlayer::State"
+QT_MOC_LITERAL(11, 121, 8) // "newState"
 
     },
     "UIController\0startGame\0\0replaceCards\0"
     "processBetValue\0newGame\0saveGame\0"
-    "loadGame\0exitGame"
+    "loadGame\0exitGame\0stateChanged\0"
+    "QMediaPlayer::State\0newState"
 };
 #undef QT_MOC_LITERAL
 
@@ -52,7 +56,7 @@ static const uint qt_meta_data_UIController[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       7,   14, // methods
+       8,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -60,13 +64,14 @@ static const uint qt_meta_data_UIController[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   49,    2, 0x0a /* Public */,
-       3,    0,   50,    2, 0x0a /* Public */,
-       4,    0,   51,    2, 0x0a /* Public */,
-       5,    0,   52,    2, 0x0a /* Public */,
-       6,    0,   53,    2, 0x0a /* Public */,
-       7,    0,   54,    2, 0x0a /* Public */,
-       8,    0,   55,    2, 0x0a /* Public */,
+       1,    0,   54,    2, 0x0a /* Public */,
+       3,    0,   55,    2, 0x0a /* Public */,
+       4,    0,   56,    2, 0x0a /* Public */,
+       5,    0,   57,    2, 0x0a /* Public */,
+       6,    0,   58,    2, 0x0a /* Public */,
+       7,    0,   59,    2, 0x0a /* Public */,
+       8,    0,   60,    2, 0x0a /* Public */,
+       9,    1,   61,    2, 0x0a /* Public */,
 
  // slots: parameters
     QMetaType::Void,
@@ -76,6 +81,7 @@ static const uint qt_meta_data_UIController[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 10,   11,
 
        0        // eod
 };
@@ -93,10 +99,21 @@ void UIController::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id
         case 4: _t->saveGame(); break;
         case 5: _t->loadGame(); break;
         case 6: _t->exitGame(); break;
+        case 7: _t->stateChanged((*reinterpret_cast< QMediaPlayer::State(*)>(_a[1]))); break;
         default: ;
         }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 7:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QMediaPlayer::State >(); break;
+            }
+            break;
+        }
     }
-    Q_UNUSED(_a);
 }
 
 const QMetaObject UIController::staticMetaObject = {
@@ -124,13 +141,13 @@ int UIController::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 7)
+        if (_id < 8)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 7;
+        _id -= 8;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 7)
-            *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 7;
+        if (_id < 8)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 8;
     }
     return _id;
 }
--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
 git add --all
 git config --global user.name "Hao Huynh"
 git config --global user.email hah.ptit@gmail.com
-git commit -m "Flapping Bird v2: Complete MySQL"
+git commit -m "Flapping Bird v2: Interleaved flowers"
 git push origin master
