a565f1817abe9462199c93ca08e90849ff8054cc
Hao Huynh <hah.ptit@gmail.com>
2015-12-10 14:39:21 -0800

    Poker: write-up : Pseudo code

--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
 git add --all
 git config --global user.name "Hao Huynh"
 git config --global user.email hah.ptit@gmail.com
-git commit -m "Poker: write-up on processing"
+git commit -m "Poker: write-up : Pseudo code "
 git push origin master
