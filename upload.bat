8dd8ab4c5db69c9858b2e786bf41e7945bc98b2c
Hao Huynh <hah.ptit@gmail.com>
2015-12-10 10:39:40 -0800

    Poker: write-up on processing

--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
 git add --all
 git config --global user.name "Hao Huynh"
 git config --global user.email hah.ptit@gmail.com
-git commit -m "CIS 17B: BaseLine"
+git commit -m "Poker: write-up on processing"
 git push origin master
